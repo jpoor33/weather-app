76cb285b88e89887c3d773a8ab3ee174c77f830e
jpoor33 <74677747+jpoor33@users.noreply.github.com>
2025-01-25 15:12:15 -0500

    final change to html route??

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -8,5 +8,5 @@ const router = Router();
 
 // TODO: Define route to serve index.html
 router.get('/', (_req,res) =>
-res.sendFile(path.join(__dirname, '..', 'index.html')))
+res.sendFile(path.join(__dirname, '..', '../../../client/dist/index.html')))
 export default router;
